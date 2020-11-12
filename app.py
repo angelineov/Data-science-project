adce5db855bcc2a5c978d556e0c5b3303da1ec78
Angeline <65604486+angelineov@users.noreply.github.com>
2020-11-12 11:53:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -245,4 +245,4 @@ def update_graph3(option_slctd):
     return container, fig
 
 if __name__ == '__main__':
-    app.run_server(host='127.0.0.1', debug=True)
+    app.run_server(host='127.0.0.1', debug=False)
